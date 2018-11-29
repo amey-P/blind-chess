e39604fc21f29033b65b71b43c0b87706a9400d5
Amey Patil <amey-P@github.com>
2018-11-30 03:43:40 +0530

    Display formatting

--- main.py
+++ main.py
@@ -4,17 +4,18 @@ import sys,configparser
 import chess
 import play
 
-WELCOME_MSG = """Welcome to blind-chess v0.01
-Type "help" for more information
+WELCOME_MSG = """Welcome to blind-chess v0.01"""
+NEW_GAME = """Type "help" for more information
 Type "new" for new a new game"""
 HELP = """new/n : New game
 quit/q/exit : Quit
 """
-move_counter = 1
-white = True
+
+match = None
 
 def listener():
     while True:
+        print(NEW_GAME)
         inp = input().strip()
         if inp == 'help' or inp == 'h':
             print(HELP)
@@ -27,6 +28,7 @@ def listener():
 
 
 def new_game():
+    global match
     white=None
     black=None
     try:
@@ -46,36 +48,66 @@ def new_game():
     try:
         while True:
             black = input("Black Player (engine/e or human/h/u): ").strip()
-            if black == 'e' or white == 'engine':
+            if black == 'e' or black == 'engine':
                 engine_name = input('Enter engine name : ')
                 black=engine_name.upper()
                 raise 
-            elif black == "human" or white == "h" or white == "u":
+            elif black == "human" or black == "h" or black == "u":
                 black='human'
                 raise 
             else:
                 print("Wrong Entry!")
     except:
         pass
-    match = play.Match(white,black)
+    print('\x1b[2J\x1b[2K\x1b[0;0f',end='')
+    match = play.Match(white,black, display=display, input_function=inp_function, error=error)
     verdict = match.play()
     if verdict == 'draw':
-        print("Its a draw.")
+        print("\nIts a draw.")
     elif verdict == 'white':
-        print("White won!")
+        print("\nWhite won!")
     elif verdict == 'black':
-        print("black won!")
+        print("\nBlack won!")
 
-def display(text):
-    global white, move_counter
+def inp_function():
+    global match
+    white = match.board.turn
+    move_counter = len(match.board.move_stack)+1
+    move_counter = move_counter//2 + move_counter%2
+    if not move_counter:
+        move_counter=1
     if white:
-        print('{0}'.format(move_counter),end='')
+        print('{0}.\t'.format(move_counter),end='')
+    else:
+        print("\t",end="")
+    move = input()
+    rewrite()
+    return move
+
+def rewrite():
+    global match
+    white = True
+    moves = match.moves
+    print('\x1b[2J\x1b[2K\x1b[0;0f',end='')
+    for x,num in zip(moves,range(1,len(moves)+1)):
+        display(x, white, num//2+num%2)
+        white = not white
+
+def display(text, white=None, move_counter=None):
+    global match
+    if white is None:
+        white = not match.board.turn
+    if move_counter is None:
+        move_counter = len(match.board.move_stack)//2+ len(match.board.move_stack)%2
+    if white:
+        print('{0}.'.format(move_counter),end='')
     print('\t{0}'.format(text),end='')
     if not white:
         print()
-        move_counter+=1
-    white = not white
 
+def error(msg):
+    if msg=="Invalid Move":
+        return rewrite()
 
 if __name__ == '__main__':
     print(WELCOME_MSG)
--- play.py
+++ play.py
@@ -14,6 +14,7 @@ class Match:
 
         self.white = white
         self.black = black
+        self.board=chess.Board()
         if board:
             self.board = board
         self.display = display
@@ -21,12 +22,13 @@ class Match:
         self.error = error
         self.input_function = input_function
 
-    def get_move(self,engine):
+    def get_engine_move(self,engine):
         settings = self.config[engine]
         engine_loc = settings.pop('Location')
         engine = uci.popen_engine(engine_loc)
         engine.position(self.board)
         move, ponder = engine.go(**settings)
+        engine.terminate()
         settings['Location'] = engine_loc
         return move
 
@@ -34,30 +36,40 @@ class Match:
         while not (self.board.is_game_over() or self.board.can_claim_draw()):
             if self.board.turn is chess.WHITE:
                 if self.white == 'human':
+                    inp = ""
                     while True:
                         try:
-                            move = self.board.parse_san(self.input_function())
+                            inp = self.input_function()
+                            move = self.board.parse_san(inp)
                             break
                         except:
-                            error("Invalid Move")
+                            self.error("Invalid Move")
                     self.board.push(move)
+                    self.display(inp)
+                    self.moves.append(inp)
                 else:
-                    move = self.get_move(self.white)
+                    move = self.get_engine_move(self.white)
                     san_move = self.board.san(move)
+                    self.moves.append(san_move)
                     self.board.push(move)
                     self.display(san_move)
             elif self.board.turn is chess.BLACK:
                 if self.black == 'human':
+                    inp = ""
                     while True:
                         try:
-                            move = self.board.parse_san(self.input_function())
+                            inp = self.input_function()
+                            move = self.board.parse_san(inp)
                             break
                         except:
-                            error("Invalid Move")
+                            self.error("Invalid Move")
                     self.board.push(move)
+                    self.display(inp)
+                    self.moves.append(inp)
                 else:
-                    move = self.get_move(self.black)
+                    move = self.get_engine_move(self.black)
                     san_move = self.board.san(move)
+                    self.moves.append(san_move)
                     self.board.push(move)
                     self.display(san_move) 
         if self.board.is_stalemate() or self.board.can_claim_draw():
